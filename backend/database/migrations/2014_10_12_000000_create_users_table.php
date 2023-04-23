1bd3edad19b1ddcab20a6e9cfeda7edcee86e9ae
Flore Sakowski <flore@sakowski.blue>
2023-04-23 08:41:40 +0200

    create_migration

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('age')->default(12);
+            $table->unsignedBigInteger('question_id');
         });
     }
 
